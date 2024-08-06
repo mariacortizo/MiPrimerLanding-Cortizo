0d86549a7ca8e518ba47435968cd4724ea7f01bd
Maria Cortizo <alemacortizo@gmail.com>
2024-08-06 09:58:04 -0300

    Agregue link inicio

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,13 +7,16 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
     return (
         <nav className='navbar'>
-            <h2 className='titulo'>Tendencia</h2>
-            <CartWidget />
+            
+            
+            <Link to="/"> Inicio </Link>
             <Link to="/categoria/pantalones">Pantalones</Link>
             <Link to="/categoria/remeras">Remeras</Link>
             <Link to="/categoria/sacos">Sacos</Link>
             <Link to="/categoria/sweters">Sweters</Link>
             <Link to="/categoria/vestidos">Vestidos</Link>
+            <h2 className='titulo'>Tendencia</h2>
+            <CartWidget />
         </nav>
     );
 }
